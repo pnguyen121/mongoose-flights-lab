dca79534271770a1dc680478aacfb90a902e37e3
pnguyen121 <phillynguyen17@gmail.com>
2022-11-29 15:21:02 -0500

    added in database and is working in client

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+// require in the database so we can use it
+require('./config/database');
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
